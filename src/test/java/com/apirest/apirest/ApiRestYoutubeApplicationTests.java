f1c9ffbcecd4f7af061f044a8400beaf9e59541e
yeisonsanz <yeisonkb@gmail.com>
2024-01-21 12:43:04 -0500

    inicio del proyecto

---
+++ src/test/java/com/apirest/apirest/ApiRestYoutubeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apirest.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestYoutubeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
